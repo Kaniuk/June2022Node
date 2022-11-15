4863d3f5212e49c698069d794bf741887cc53cc5
Liudmyla <lyudmula1989@ukr.net>
2022-11-15 12:32:46 +0200

    Add some changes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 const fs = require('node:fs');
 
 
@@ -28,4 +29,29 @@ fs.readdir('./boys', {withFileTypes: true}, (err, files) => {
             }
         });
     }
-});
+});*/
+const fs = require('fs/promises');
+const path = require('path');
+
+const sorter = async (readFolder, writeFolder, gender) => {
+    try {
+        const folderPath = path.join(__dirname, readFolder);
+
+        const files = await fs.readdir(folderPath);
+
+        for (const file of files) {
+            const filePath = path.join(folderPath, file);
+            const data = await fs.readFile(filePath);
+            const user = JSON.parse(data);
+
+            if (user.gender === gender) {
+                await fs.rename(filePath, path.join(__dirname, writeFolder, file));
+            }
+        }
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+sorter('boys', 'girls', 'female');
+sorter('girls', 'boys', 'male');
