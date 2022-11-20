6058a60c5adc2764c42dace62fdbeed5e6bbb750
Liudmyla <lyudmula1989@ukr.net>
2022-11-20 17:20:48 +0200

    Add some changes

--- app.js
+++ app.js
@@ -1,74 +1,24 @@
 const express = require('express');
+require('dotenv').config();
 
-const PORT = 5000;
+const userRouter = require('./router/user.router');
+const configs = require('./config/config');
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-const userDb = require('./dataBase/users');
+app.use('/users', userRouter);
 
-app.get('/users', (req, res) => {
-    console.log('Users endpoint');
-    res.json(userDb);
-});
-app.get('/users/:userId', (req, res) => {
-    const {userId} = req.params;
-
-    if (isNaN(userId)) {
-        res.status(400).json({msg: `The request cannot be fulfilled due to bad syntax.`});
-    } else if (userId !== userDb[userId]) {
-        res.status(404).json({msg: `The user with index is not: ${userId} found`});
-
-    }
-
-    res.json(userDb[userId]);
-});
-app.post('/users', (req, res) => {
-    const userInfo = req.body;
-    userDb.push(userInfo);
-
-    if (!userInfo.name) {
-        res.status(400).json({msg: `Pleas include name`});
-    } else if (!userInfo.age) {
-        res.status(400).json({msg: `Pleas include age`});
-    }
-
-    res.status(201).json('Created');
-});
-app.put('users/:userId', (req, res) => {
-    const newUserInfo = req.body;
-    const userId = req.params.userId;
-
-    if (isNaN(userId)) {
-        res.status(400).json({msg: `The request cannot be fulfilled due to bad syntax.`});
-    }
-
-    if (!newUserInfo.name) {
-        res.status(400).json({msg: `Pleas include name`});
-    } else if (!newUserInfo.age) {
-        res.status(400).json({msg: `Pleas include age`});
-    }
-
-    userDb[userId] = newUserInfo;
-
-    res.json('Updated');
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message || 'Unknown error',
+        status: err.status || 500
+    });
 });
 
-app.delete('/users/:userId', (req, res) => {
-    const {userId} = req.params;
-    if (isNaN(userId)) {
-        res.status(400).json({msg: `The request cannot be fulfilled due to bad syntax.`});
-    }
-    if (!userDb[userId]) {
-        res.status(400).json({msg: `No user with id of ${userId}`});
-    } else {
-        userDb.splice(+userId, 1);
-    }
-    res.json('Deleted');
+app.listen(configs.PORT, () => {
+    console.log(`Server listen ${configs.PORT}`);
 });
 
-
-app.listen(PORT, () => {
-    console.log(`Server listen ${PORT}`);
-});
